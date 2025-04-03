709b0597255d3f0fc746d7beb047553713e40e16
mac <mac@macs-MacBook-Pro.local>
2025-04-03 09:23:23 +0430

    feat: create sample web server

--- starter/index.js
+++ starter/index.js
@@ -1,18 +1,31 @@
-const fs = require('fs');
+import { readFileSync, writeFileSync, readFile, writeFile } from 'fs';
+import { createServer } from 'http'; 
+
+
+///////////////////////////////////FILE///////////////////////////
 
 //Blocking, Synchonous way
-const fileContent = fs.readFileSync('./starter/txt/start.txt', 'utf-8');
-console.log(fileContent);
+// const fileContent = readFileSync('./starter/txt/start.txt', 'utf-8');
+// console.log(fileContent);
 
-const newFileContent = `This is all we know about Avacado:\n ${fileContent}`;
-fs.writeFileSync('./starter/txt/output1.txt', fileContent);
-console.log('File written');
+// const newFileContent = `This is all we know about Avacado:\n ${fileContent}`;
+// writeFileSync('./starter/txt/output1.txt', fileContent);
+// console.log('File written');
 
-//None Bloking, Asynchronous way
-fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data) => {
-    fs.writeFile('./starter/txt/output2.txt',  `New file content: ${data}`, 'utf-8', err => {
-        console.log('Done!');
-    })
+// //None Bloking, Asynchronous way
+// readFile('./starter/txt/start.txt', 'utf-8', (err, data) => {
+//     writeFile('./starter/txt/output2.txt',  `New file content: ${data}`, 'utf-8', err => {
+//         console.log('Done!');
+//     })
+// })
+
+// console.log('Will read the FIle');
+
+///////////////////////////////////SERVER///////////////////////////
+const server = createServer((req, res) => {
+    res.end('Hello from Node Server!')
 })
 
-console.log('Will read the FIle');
+server.listen(3000, 'localhost', () => {
+    console.log('Sever started at port 3000')
+})
