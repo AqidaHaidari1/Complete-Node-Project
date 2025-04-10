5683cc122d59002efe98cb65bc49c5495267bf7f
mac <mac@macs-MacBook-Pro.local>
2025-04-10 18:23:06 +0430

    connect to mongoDB

--- natours/app.js
+++ natours/app.js
@@ -2,26 +2,30 @@ import express from "express";
 import morgan from "morgan";
 import tourRouter from "./route/tourRoute.js";
 import userRouter from "./route/userRoute.js";
+import { configDotenv } from "dotenv";
+configDotenv();
 
 const app = new express();
 
 //middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(express.static(`./public`));
 
 app.use((req, res, next) => {
-	console.log("hello from middleware");
-	next();
+  console.log("hello from middleware");
+  next();
 });
 app.use((req, res, next) => {
-	req.currentTime = new Date().toISOString();
-	console.log(req.currentTime);
-	next();
+  req.currentTime = new Date().toISOString();
+  console.log(req.currentTime);
+  next();
 });
-
 
 //routes
-app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/tours/", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 export default app;
--- natours/server.js
+++ natours/server.js
@@ -1,6 +1,21 @@
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+configDotenv();
+
 import app from "./app.js";
 
-const port = 3000;
+const db_password = process.env.DATABASE_PASSWORD;
+const db = process.env.DATABASE.replace("<PASSWORD>", db_password);
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log("DB connected successfuly!");
+  });
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log("App is running on port 3000");
+  console.log(`App is running on port ${port}`);
 });
