872bb83bf7021e3f47951aa5941d67b25a51b4db
mac <mac@macs-MacBook-Pro.local>
2025-04-13 08:50:04 +0430

    feat: add filtring to query

--- natours/controller/tourController.js
+++ natours/controller/tourController.js
@@ -2,7 +2,17 @@ import Tour from "../models/tourModel.js";
 
 export const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // Execute Query
+    const tours = await query;
+
+    //Send Response
     res.status(200).json({
       status: "success",
       currentTime: req.currentTime,
