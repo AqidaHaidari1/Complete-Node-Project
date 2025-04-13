9dac4c563ecc8ee3b03f63b9086c1afd7fb76226
mac <mac@macs-MacBook-Pro.local>
2025-04-13 13:20:44 +0430

    feat: add pagination

--- natours/controller/tourController.js
+++ natours/controller/tourController.js
@@ -40,10 +40,18 @@ export const getAllTours = async (req, res) => {
     } else {
       query = query.select("-__v"); // excluding __v field
     }
-
+	  //paganition
+	  const page = req.query.page * 1 || 1;
+	  const limit = req.query.limit * 1 || 100;
+	  const skip = (page - 1) * limit;
+	  query = query.skip(skip).limit(limit)
     // Execute Query
     const tours = await query;
-
+	  if (req.query.page) {
+		  const numTours = await Tour.countDocuments()
+		  if(skip >= numTours) throw new Error("this page doesn't exist");
+		  
+	}
     //Send Response
     res.status(200).json({
       status: "success",
