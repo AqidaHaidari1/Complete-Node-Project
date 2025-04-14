e8abfd9e39aa6451f369b8f61e9ca0740510254d
mac <mac@macs-MacBook-Pro.local>
2025-04-14 09:19:18 +0430

    feat: aggregation pipline

--- natours/controller/tourController.js
+++ natours/controller/tourController.js
@@ -161,12 +161,91 @@ export const checkID = (req, res, next, val) => {
   // next()
 };
 
-// export const checkBody = (req, res, next) => {
-//     if (!req.body.name || !req.body.price) {
-//         return res.status(404).json({
-//             message: 'Missing Price Or Name!'
-//         })
-//     }
+export const getToursStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.7 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: "$difficulty" },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: "$ratingsQuantity" },
+          avgRating: { $avg: "$ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: {
+            $min: "$price",
+          },
+          maxPrice: { $max: "$price" },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      {
+        $match: { _id: { $ne: "EASY" } },
+      },
+    ]);
+    console.log(stats, "statas");
+    res.status(200).json({
+      status: "success",
+      result: stats.length,
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-//     next()
-// }
+export const getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    console.log(year);
+    const plan = await Tour.aggregate([
+      { $unwind: "$startDates" },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: "$name" },
+        },
+      },
+      {
+        $addFields: { month: "$_id" },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+    res.status(200).json({
+      status: "success",
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
--- natours/route/tourRoute.js
+++ natours/route/tourRoute.js
@@ -8,9 +8,13 @@ import {
   updateTour,
   deleteTour,
   aliasTopTours,
+    getToursStats,
+  getMonthlyPlan
 } from "../controller/tourController.js";
 
 // router.param('id', checkID)
+router.route("/get-monthly-plan/:year").get(getMonthlyPlan);
+router.route("/get-tours-state").get(getToursStats);
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 router.route("/").get(getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
