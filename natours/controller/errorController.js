ab0a0aebbfff16208ba3e0d266b6810b82a7b85b
mac <mac@macs-MacBook-Pro.local>
2025-04-15 12:46:39 +0430

    handle mongoose validation errors

--- natours/controller/errorController.js
+++ natours/controller/errorController.js
@@ -1,23 +1,50 @@
-const sendErrorDev = (res, err) => {
+import AppError from "../utils/appError.js";
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  console.log("excecuted!");
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    message: err.message,
     error: err,
+    message: err.message,
     stack: err.stack,
   });
 };
 
-const sendErrorProd = (res, err) => {
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
+
+    // Programming or other unknown error: don't leak error details
   } else {
-    console.log("ERROR!");
+    // 1) Log error
+    console.error("ERROR!", err);
+
+    // 2) Send generic message
     res.status(500).json({
-      status: err.status,
-      message: err.message,
+      status: "error",
+      message: "Something went very wrong!",
     });
   }
 };
@@ -27,8 +54,16 @@ export const globalErrorController = (err, req, res, next) => {
   err.status = err.status || "error";
 
   if (process.env.NODE_ENV === "development") {
-    sendErrorDev(res, err);
+    sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    sendErrorProd(res, err);
+    let error = Object.create(err);
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    console.log(error, "here");
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
