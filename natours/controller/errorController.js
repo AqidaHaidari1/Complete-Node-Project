40610ae40591b3fb1917ff2daf76533d78f770ff
mac <mac@macs-MacBook-Pro.local>
2025-04-15 10:52:58 +0430

    handle error in Prod & Dev

--- natours/controller/errorController.js
+++ natours/controller/errorController.js
@@ -1,8 +1,34 @@
-export const globalErrorController = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+const sendErrorDev = (res, err) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    error: err,
+    stack: err.stack,
   });
+};
+
+const sendErrorProd = (res, err) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log("ERROR!");
+    res.status(500).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+};
+
+export const globalErrorController = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(res, err);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(res, err);
+  }
 };
