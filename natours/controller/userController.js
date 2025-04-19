af111dc4d7e18cfafa45bc647d0ffceb4b3dcc14
mac <mac@macs-MacBook-Pro.local>
2025-04-19 13:13:29 +0430

    feat: update current user data

--- natours/controller/userController.js
+++ natours/controller/userController.js
@@ -1,30 +1,66 @@
+import User from "../models/userModel.js";
+import AppError from "../utils/appError.js";
+import { catchAsync } from "../utils/catchAsync.js";
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+export const updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not assword upadte, please use this route /updateMyPassword",
+        400,
+      ),
+    );
+  }
+
+  const filtredBody = filterObj(req.body, "name", "email");
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filtredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 export const getAllUsers = (req, res) => {
-	res.status(500).json({
-		status: "err",
-		message: "This rout not yet defined!",
-	});
+  res.status(500).json({
+    status: "err",
+    message: "This rout not yet defined!",
+  });
 };
 export const getUser = (req, res) => {
-	res.status(500).json({
-		status: "err",
-		message: "This rout not yet defined!",
-	});
+  res.status(500).json({
+    status: "err",
+    message: "This rout not yet defined!",
+  });
 };
 export const updateUser = (req, res) => {
-	res.status(500).json({
-		status: "err",
-		message: "This rout not yet defined!",
-	});
+  res.status(500).json({
+    status: "err",
+    message: "This rout not yet defined!",
+  });
 };
 export const deleteUser = (req, res) => {
-	res.status(500).json({
-		status: "err",
-		message: "This rout not yet defined!",
-	});
+  res.status(500).json({
+    status: "err",
+    message: "This rout not yet defined!",
+  });
 };
 export const createUser = (req, res) => {
-	res.status(500).json({
-		status: "err",
-		message: "This rout not yet defined!",
-	});
+  res.status(500).json({
+    status: "err",
+    message: "This rout not yet defined!",
+  });
 };
--- natours/route/userRoute.js
+++ natours/route/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 
-import {getAllUsers, getUser, createUser, updateUser, deleteUser} from '../controller/userController.js'
+import {getAllUsers, getUser, createUser, updateUser, deleteUser, updateMe} from '../controller/userController.js'
 import { signUp, login, forgotPassword, resetPassword, protect, updatePassword } from "../controller/authConroller.js";
+
 
 router.route("/signup").post(signUp);
 router.route("/login").post(login);
@@ -10,6 +11,8 @@ router.route("/login").post(login);
 router.route("/forgotPassword").post(forgotPassword);
 router.route("/resetPassword/:token").patch(resetPassword);
 router.route("/updateMypassword").patch(protect, updatePassword);
+
+router.route("/updateMe").patch(protect, updateMe);
 
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
