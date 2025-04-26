98ca34dd7f855bb4792e9ce88f7911f7647bbba9
mac <mac@MacBook-Pro.local>
2025-04-26 09:23:59 +0430

    import user, review, tour data to DB

--- natours/dev-data/data/import-dev-data.js
+++ natours/dev-data/data/import-dev-data.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import { configDotenv } from "dotenv";
 import Tour from "../../models/tourModel.js";
+import User from "../../models/userModel.js";
+import Review from "../../models/reviewTour.js";
+
 import fs from "fs";
 
 configDotenv();
@@ -20,11 +23,19 @@ mongoose
 const tours = JSON.parse(
   fs.readFileSync(`./dev-data/data/tours.json`, "utf-8"),
 );
+const users = JSON.parse(
+  fs.readFileSync(`./dev-data/data/users.json`, "utf-8"),
+);
+const reviews = JSON.parse(
+  fs.readFileSync(`./dev-data/data/reviews.json`, "utf-8"),
+);
 
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, {validateBeforeSave: false});
+    await Review.create(reviews);
     console.log("Data successfully loaded!");
   } catch (err) {
     console.log(err);
@@ -36,6 +47,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data successfully deleted!");
   } catch (err) {
     console.log(err);
