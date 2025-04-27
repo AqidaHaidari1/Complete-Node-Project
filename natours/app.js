cc8d9d9c95eb6002bb4eacf9bf4e95fc641f2b05
mac <mac@MacBook-Pro.local>
2025-04-27 13:08:40 +0430

    basics of pug

--- natours/app.js
+++ natours/app.js
@@ -73,7 +73,10 @@ app.use((req, res, next) => {
 
 //routes
 app.get("/", (req, res) => {
-  res.status(200).render("base");
+  res.status(200).render("base", {
+    tour: "The Forest Hicker",
+    user: "John",
+  });
 });
 
 app.use("/api/v1/tours", tourRouter);
